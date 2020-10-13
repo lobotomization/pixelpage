44ad41bc2150bcecf5a2fde2e1a4399d8b5ab438
lobotomization <lobotomization@gmail.com>
2020-10-13 04:52:09 +0000

    Make addRoutes.sh idempotent

--- pixelart/addRoutes.sh
+++ pixelart/addRoutes.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 VER=$1
 
-sed -i "1s/^/from . import "$VER"\n/" __init__.py
-
+if grep -q 'import '$VER __init__.py
+then
+	echo "$VER.py already imported into __init__.py"
+else
+	sed -i "1s/^/from . import "$VER"\n/" __init__.py
+	echo "$VER.py imported into __init__.py"
+fi
 
 ROUTES=(`grep route "$VER.py" | cut -d '"' -f 2`)
 for route in "${ROUTES[@]}"
@@ -10,6 +15,12 @@ do
 	FUNC=`grep 'route("'"$route"'"' "$VER.py" -A 1 | grep def | cut -d ' ' -f 2 | tr -d '():'`
 	LINENUM=`grep -n add_url_rule __init__.py | tail -n 1 | cut -d ':' -f 1`
 	(( LINENUM++ ))
-	sed -i $LINENUM"i app.add_url_rule(\"$route\", view_func=$VER.$FUNC)" __init__.py
-	echo "Route "$route" linked to view function "$FUNC" on line "$LINENUM" of __init__.py."
+	if grep -q 'app.add_url_rule("'$route'"' __init__.py 
+	then
+		echo "Route "$route" already linked to view function in __init__.py."
+	else
+		sed -i $LINENUM"i app.add_url_rule(\"$route\", view_func=$VER.$FUNC)" __init__.py
+		echo "Route "$route" linked to view function "$FUNC" on line "$LINENUM" of __init__.py."
+
+	fi
 done
